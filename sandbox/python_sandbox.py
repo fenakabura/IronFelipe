f06606855b9222ada88fb19c9968443d65fb313e
fenakabura <108941144+fenakabura@users.noreply.github.com>
2022-08-29 11:11:52 +0200

    updates

---
+++ sandbox/python_sandbox.py
@@ -0,0 +1,14 @@
+from random import randrange, random
+
+def main():
+    print("This is the python_sandbox.py file.")
+    x = randrange(1, 101)
+    print(x)
+    y = random()
+    print(y)
+
+main()
+
+#%%
+
+#%%
